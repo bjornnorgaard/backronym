957976451eb7e751911609ff35efe807f7df8d64
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-09-04 17:14:41 +0200

    Setup skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+<button type="button" class="btn-icon variant-filled">(icon)</button>
+<button type="button" class="btn variant-filled">Button</button>
+<button type="button" class="btn variant-filled">
+    <span>(icon)</span>
+    <span>Button</span>
+</button>
