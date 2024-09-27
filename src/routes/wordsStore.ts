175730cfe5c94f7bfd96b69d064bd2cde4997b69
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-09-27 11:53:29 +0200

    Fixed goof in domain ordering

--- src/routes/wordsStore.ts
+++ src/routes/wordsStore.ts
@@ -2,5 +2,5 @@ import { writable } from "svelte/store";
 import { technical } from "./technical";
 import { humanitarian } from "./humanitarian";
 
-export const domains = [technical, humanitarian];
+export const domains = [humanitarian, technical];
 export const words = writable<string[]>(domains[0]);
